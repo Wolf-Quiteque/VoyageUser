9ce847e29efd4e4421d91503fe56371ecf451110
Wolf-Quiteque <marcioqui3@gmail.com>
2024-08-09 14:44:54 +0100

    update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import SignUpScreen from './screens/SignUpScreen'; // Default import
-
+import SignUpScreen from './screens/SignUpScreen.js'
 export default function App() {
   return (
     <View style={{ flex: 1 }}> {/* Make sure the View takes up the full screen */}
